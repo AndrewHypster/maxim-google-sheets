ebaf919e62901fd34b55980aeadb863af80c7cf4
Андрій Гречух <119750751+AndrewHypster@users.noreply.github.com>
2025-08-22 11:14:07 +0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,4 +1,14 @@
 export default async function handler(req, res) {
+  // дозволяємо CORS для будь-яких сайтів
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    // відповідаємо одразу на preflight-запит
+    return res.status(200).end();
+  }
+
   if (req.method === "POST") {
     try {
       const GOOGLE_SCRIPT_URL = process.env.API_URL;
